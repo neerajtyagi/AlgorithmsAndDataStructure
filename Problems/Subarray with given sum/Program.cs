dc32f37583585d16eefce7ccf55134255cdc0d43
Neeraj Tyagi <ntyagi@microsoft.com>
2020-09-14 16:56:26 +0530

    Solution complete

---
+++ Problems/Subarray with given sum/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Subarray_With_Given_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<SubArray> data = new List<SubArray>();
+
+            string inputLenght = Console.ReadLine();
+            int length = int.Parse(inputLenght);
+            for (int i = 0; i < length; i++)
+            {
+                string inputRange = Console.ReadLine();
+                string inputData = Console.ReadLine();
+                data.Add(new SubArray(inputRange, inputData));
+            }
+
+            foreach (var item in data)
+            {
+                item.Solve();
+            }
+        }
+    }
+}
---
+++ Problems/Subarray with given sum/SubArray.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+
+namespace Subarray_With_Given_Sum
+{
+    public class SubArray
+    {
+        public SubArray(string format, string array)
+        {
+            // Check format for range and sum
+            string[] formatArray = format.Split(' ');
+            this.ArrayLength = int.Parse(formatArray[0]);
+            this.ArraySum = int.Parse(formatArray[1]);
+
+            // Check format for array data
+            string[] formatData = array.Split(' ');
+            this.Array = formatData.Select(x => int.Parse(x)).ToArray();
+        }
+
+        public int ArrayLength { get; set; }
+        public int ArraySum { get; set; }
+        public int[] Array { get; set; }
+
+        public void Solve()
+        {
+            int startIndex = 0;
+            int currentIndex = 0;
+            int sum = 0;
+            while (startIndex < ArrayLength)
+            {
+                sum = 0;
+                currentIndex = startIndex;
+                while (currentIndex < ArrayLength && sum < ArraySum)
+                {
+                    sum += Array[currentIndex];
+                    currentIndex++;
+                }
+
+                if(sum == ArraySum)
+                {
+                    break;
+                }
+
+                startIndex++;
+            };
+
+            if (sum == ArraySum)
+            {
+                Console.WriteLine("{0} {1}", startIndex + 1, currentIndex);
+            }
+            else
+            {
+                Console.WriteLine("-1");
+            }
+        }
+    }
+}
