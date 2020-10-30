b5b8d8ddee157921344b68e9e07f46d3f3f8dc29
Neeraj Tyagi <ntyagi@microsoft.com>
2020-10-30 23:19:06 +0530

    Implicit array used

--- Problems.Tests/AverageWeightOnBridge_Tests.cs
+++ Problems.Tests/AverageWeightOnBridge_Tests.cs
@@ -5,7 +5,7 @@ namespace Problems_Tests
 {
     public class AverageWeightOnBridge_Tests
     {
-        private int[,] data = new int[,] { { 0900, 1200, 200 }, { 1100, 1300, 500 }, { 1000, 1200, 200 }, { 1400, 1600, 1100 } };
+        private int[,] data = new [,] { { 0900, 1200, 200 }, { 1100, 1300, 500 }, { 1000, 1200, 200 }, { 1400, 1600, 1100 } };
 
         [Fact]
         public void TestAt_1000()
