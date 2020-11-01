c7d48118e8068140d948c3ed0f7cc44f15dfe363
Neeraj Tyagi <ntyagi@microsoft.com>
2020-11-01 21:39:33 +0530

    boundary conditions added

--- Problems.Tests/AverageWeightOnBridge_Tests.cs
+++ Problems.Tests/AverageWeightOnBridge_Tests.cs
@@ -5,7 +5,7 @@ namespace Problems_Tests
 {
     public class AverageWeightOnBridge_Tests
     {
-        private int[,] data = new [,] { { 0900, 1200, 200 }, { 1100, 1300, 500 }, { 1000, 1200, 200 }, { 1400, 1600, 1100 } };
+        private int[,] data = new[,] { { 0900, 1200, 200 }, { 1100, 1300, 500 }, { 1000, 1200, 200 }, { 1400, 1600, 1100 }, { 2000, 0200, 700 } };
 
         [Fact]
         public void TestAt_1000()
@@ -19,6 +19,20 @@ namespace Problems_Tests
         {
             int averageWeight = new Problems.AverageWeightOnBridge().GetAverageWeightAtTime(data, 1100);
             Assert.Equal(averageWeight, 300);
+        }
+
+        [Fact]
+        public void TestAt_2100()
+        {
+            int averageWeight = new Problems.AverageWeightOnBridge().GetAverageWeightAtTime(data, 2100);
+            Assert.Equal(averageWeight, 700);
+        }
+
+        [Fact]
+        public void TestAt_0100()
+        {
+            int averageWeight = new Problems.AverageWeightOnBridge().GetAverageWeightAtTime(data, 0100);
+            Assert.Equal(averageWeight, 700);
         }
     }
 }
--- Problems/AverageWeightOnBridge.cs
+++ Problems/AverageWeightOnBridge.cs
@@ -9,7 +9,8 @@ namespace Problems
             int totalWeight = 0, totalCount = 0;
             for (int record = 0; record < data.GetLength(0); record++)
             {
-                if (time >= data[record, 0] && time <= data[record, 1])
+                if ((time >= data[record, 0] && time <= data[record, 1]) ||
+                (data[record, 1] < data[record, 0] && (time >= data[record, 0] || time <= data[record, 1])))
                 {
                     totalWeight += data[record, 2];
                     totalCount++;
